60142a72ca823603e28230821e23866fbd4610dc
Ethan Olsen <131eolsen@gmail.com>
2024-01-22 21:28:43 -0800

    feat: keydown and keyup for keys

--- src/components/Piano.svelte
+++ src/components/Piano.svelte
@@ -1,33 +1,72 @@
 <script lang="ts">
 	import * as Tone from "tone";
 	import OctaveOffsetInput from "./OctaveOffsetInput.svelte";
+	import { onMount } from "svelte";
 
 	let octaveOffset = 3;
 	let octavesShown = 2;
 
 	const whiteKeys = ["C", "D", "E", "F", "G", "A", "B"];
 	const blackKeys = ["C#", "D#", "F#", "G#", "A#"];
+	type Key = `${
+		| (typeof whiteKeys)[number]
+		| (typeof blackKeys)[number]}${number}`;
 
-	const play = (key: string) => {
-		var synth = new Tone.PolySynth({
+	let synth: Tone.PolySynth;
+	let keysDown: Record<Key, boolean> = {};
+	let leftMouseDown = false;
+
+	onMount(() => {
+		synth = new Tone.PolySynth({
 			maxPolyphony: 8,
+			voice: Tone.Synth,
 			volume: -12,
+			options: { oscillator: { type: "sine" } },
 		}).toDestination();
-		synth.triggerAttackRelease(key, "8n");
+	});
+
+	const keyDown = (key: Key) => {
+		synth.triggerAttack(key);
+		keysDown[key] = true;
+	};
+	const keyUp = (key: Key) => {
+		synth.triggerRelease(key);
+		keysDown[key] = false;
 	};
 </script>
 
 <div id="settings">
 	<OctaveOffsetInput bind:octavesShown bind:octaveOffset />
 </div>
-<div class="grid grid-cols-4 w-full">
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	id="piano"
+	on:mouseleave={() => {
+		leftMouseDown = false;
+	}}
+	class="grid grid-cols-4 w-full"
+>
 	{#each new Array(octavesShown) as _, i}
-		<div class="piano-container aspect-video">
+		<div class="piano-octave-container aspect-video">
 			<div class="piano-white">
 				{#each whiteKeys as key}
 					<button
-						on:click={() => play(key + (i + octaveOffset))}
-						style="grid-area: {key};"
+						on:mouseenter={() =>
+							leftMouseDown && keyDown(`${key}${i + octaveOffset}`)}
+						on:mousedown={() => {
+							keyDown(`${key}${i + octaveOffset}`);
+							leftMouseDown = true;
+						}}
+						on:mouseup={() => {
+							keyUp(`${key}${i + octaveOffset}`);
+							leftMouseDown = false;
+						}}
+						on:mouseleave={() => keyUp(`${key}${i + octaveOffset}`)}
+						style="grid-area: {key}; background-color: {keysDown[
+							`${key}${i + octaveOffset}`
+						]
+							? '#ffffff'
+							: '#f5f2e4'};"
 						class="white-key"
 					></button>
 				{/each}
@@ -35,7 +74,17 @@
 			<div class="piano-black">
 				{#each blackKeys as key}
 					<button
-						on:click={() => play(key + (i + octaveOffset))}
+						on:mouseenter={() =>
+							leftMouseDown && keyDown(`${key}${i + octaveOffset}`)}
+						on:mousedown={() => {
+							keyDown(`${key}${i + octaveOffset}`);
+							leftMouseDown = true;
+						}}
+						on:mouseup={() => {
+							keyUp(`${key}${i + octaveOffset}`);
+							leftMouseDown = false;
+						}}
+						on:mouseleave={() => keyUp(`${key}${i + octaveOffset}`)}
 						style="grid-area: {key.replace('#', 's')};"
 						class="black-key"
 					></button>
@@ -46,7 +95,10 @@
 </div>
 
 <style>
-	.piano-container {
+	#piano {
+		user-select: none;
+	}
+	.piano-octave-container {
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-template-rows: 1fr 1fr 1fr;
