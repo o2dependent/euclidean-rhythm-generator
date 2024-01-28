c1c3f254f8eb47f6ca112cda146da43b3b7e224d
Ethan Olsen <131eolsen@gmail.com>
2024-01-27 17:36:05 -0800

    refactor(oscillator): type selection and add modulation and partial controls

--- src/components/WaveChanger.svelte
+++ src/components/WaveChanger.svelte
@@ -2,56 +2,86 @@
 	import { synth } from "../lib/piano/synth.store";
 	// @ts-ignore
 	import Knob from "svelte-knob";
+	import WaveIcons from "./WaveIcons.svelte";
 
-	const OSCILLATOR_TYPES = [
+	const OSCILLATOR_WAVE_TYPES = [
 		"sine",
-		"square",
 		"sawtooth",
 		"triangle",
-		"fmsine",
-		"fmsquare",
-		"fmsawtooth",
-		"fmtriangle",
-		"amsine",
-		"amsquare",
-		"amsawtooth",
-		"amtriangle",
-		"fatsine",
-		"fatsquare",
-		"fatsawtooth",
-		"fattriangle",
+		"square",
 	] as const;
-	const OSCILLATOR_MAX_LEVEL = 32;
+	const OSCILLATOR_MODULATION_TYPES = ["", "fat", "am", "fm"] as const;
+	const OSCILLATOR_MAX_PARTIAL = 32;
 
-	let oscillatorType = OSCILLATOR_TYPES[0] as (typeof OSCILLATOR_TYPES)[number];
-	let oscillatorLevel = 1;
+	let oscillatorType =
+		OSCILLATOR_WAVE_TYPES[0] as (typeof OSCILLATOR_WAVE_TYPES)[number];
+	let oscillatorModulationIndex = 0;
+	let oscillatorPartial = 0;
 
-	const changeOscillatorType = (type: OscillatorType) => {
-		$synth?.set({ oscillator: { type } });
+	const changeOscillatorType = ({
+		type,
+		partialCount,
+		modulation,
+	}: {
+		type: (typeof OSCILLATOR_WAVE_TYPES)[number];
+		partialCount: number;
+		modulation: (typeof OSCILLATOR_MODULATION_TYPES)[number];
+	}) => {
+		$synth?.set({
+			oscillator: {
+				type: `${modulation}${type}`,
+				partialCount,
+			},
+		});
 	};
 
 	$: {
-		changeOscillatorType(
-			`${oscillatorType}${oscillatorLevel}` as OscillatorType,
-		);
+		changeOscillatorType({
+			type: oscillatorType,
+			modulation: OSCILLATOR_MODULATION_TYPES[oscillatorModulationIndex],
+			partialCount: oscillatorPartial,
+		});
 	}
+
+	const valueDisplayFunction = (v: number) =>
+		v === 0 ? "none" : OSCILLATOR_MODULATION_TYPES[v];
 </script>
 
 <div class="flex flex-col gap-1">
 	<p>Oscillator Type</p>
-	<select
-		name="oscillator-type"
-		id="oscillator-type"
-		bind:value={oscillatorType}
-	>
-		{#each OSCILLATOR_TYPES as type}
-			<option value={type}>{type}</option>
+	<div class="grid grid-cols-4">
+		{#each OSCILLATOR_WAVE_TYPES as type}
+			<button
+				class="btn"
+				class:active={type === oscillatorType}
+				on:click={() => (oscillatorType = type)}
+			>
+				<WaveIcons {type} />
+			</button>
 		{/each}
-	</select>
+	</div>
+	<p>Modulation</p>
 	<Knob
-		bind:value={oscillatorLevel}
+		bind:value={oscillatorModulationIndex}
 		step={1}
-		min={1}
-		max={OSCILLATOR_MAX_LEVEL}
+		min={0}
+		max={OSCILLATOR_MODULATION_TYPES.length - 1}
+		{valueDisplayFunction}
+	/>
+	<p>Partials</p>
+	<Knob
+		bind:value={oscillatorPartial}
+		step={1}
+		min={0}
+		max={OSCILLATOR_MAX_PARTIAL}
 	/>
 </div>
+
+<style lang="postcss">
+	.btn {
+		@apply aspect-square p-2 bg-slate-400;
+	}
+	.btn.active {
+		@apply bg-slate-500;
+	}
+</style>
