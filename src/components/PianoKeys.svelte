476a24b8748f61e3666929c7fb65e49f0b6999fc
Ethan Olsen <131eolsen@gmail.com>
2024-02-13 02:02:49 -0800

    refactor: move piano keys to separate component

---
+++ src/components/PianoKeys.svelte
@@ -0,0 +1,115 @@
+<script lang="ts">
+	import { blackKeys, whiteKeys } from "@/lib/piano/keys";
+	import type { KeyDownKeys } from "@/lib/piano/keysDown.store";
+	import { createEventDispatcher } from "svelte";
+
+	export let octavesShown: number = 1;
+	export let keysDown: Partial<Record<KeyDownKeys, boolean>>;
+	export let octaveOffset: number = 0;
+
+	const dispatch = createEventDispatcher();
+
+	const pianoMouseLeave = () => {
+		dispatch("pianoMouseLeave");
+	};
+
+	const keyMouseEnter =
+		(key: (typeof whiteKeys | typeof blackKeys)[number], i: number) => () => {
+			dispatch("keyMouseEnter", {
+				key,
+				i,
+			});
+		};
+	const keyMouseDown =
+		(key: (typeof whiteKeys | typeof blackKeys)[number], i: number) => () => {
+			dispatch("keyMouseDown", {
+				key,
+				i,
+			});
+		};
+	const keyMouseUp =
+		(key: (typeof whiteKeys | typeof blackKeys)[number], i: number) => () => {
+			dispatch("keyMouseUp", {
+				key,
+				i,
+			});
+		};
+	const keyMouseLeave =
+		(key: (typeof whiteKeys | typeof blackKeys)[number], i: number) => () => {
+			dispatch("keyMouseLeave", {
+				key,
+				i,
+			});
+		};
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div id="piano" on:mouseleave={pianoMouseLeave} class="grid grid-cols-4 w-full">
+	{#each new Array(octavesShown) as _, i}
+		<div class="piano-octave-container aspect-video">
+			<div class="piano-white">
+				{#each whiteKeys as key}
+					<button
+						on:mouseenter={keyMouseEnter(key, i)}
+						on:mousedown={keyMouseDown(key, i)}
+						on:mouseup={keyMouseUp(key, i)}
+						on:mouseleave={keyMouseLeave(key, i)}
+						style="grid-area: {key};"
+						class="white-key"
+						class:pressed={keysDown[`${key}${i + octaveOffset}`]}
+					></button>
+				{/each}
+			</div>
+			<div class="piano-black">
+				{#each blackKeys as key}
+					<button
+						on:mouseenter={keyMouseEnter(key, i)}
+						on:mousedown={keyMouseDown(key, i)}
+						on:mouseup={keyMouseUp(key, i)}
+						on:mouseleave={keyMouseLeave(key, i)}
+						style="grid-area: {key.replace('#', 's')};"
+						class="black-key"
+						class:pressed={keysDown[`${key}${i + octaveOffset}`]}
+					></button>
+				{/each}
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	#piano {
+		user-select: none;
+	}
+	.piano-octave-container {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr 1fr 1fr;
+	}
+	.piano-black {
+		grid-area: 1 / 1 / 3 / 2;
+		display: grid;
+		grid-template-areas: ". . Cs Cs . Ds Ds . . . . Fs Fs . Gs Gs . As As . .";
+	}
+	.piano-white {
+		grid-area: 1 / 1 / 4 / 2;
+		display: grid;
+		grid-template-areas: "C C C D D D E E E F F F G G G A A A B B B";
+	}
+	.white-key {
+		background-color: #f5f2e4;
+		border-radius: 0vw 0vw 0.5vw 0.5vw;
+		border: 1px solid #00000040;
+	}
+	.white-key.pressed {
+		background-color: hsl(49, 46%, 98%);
+	}
+	.black-key {
+		background-color: #000000;
+		border-radius: 0vw 0vw 0.5vw 0.5vw;
+		border: 1px solid #ffffff40;
+	}
+	.black-key.pressed {
+		background-color: hsl(0, 0%, 14%);
+	}
+</style>
--- src/components/piano/Piano.svelte
+++ src/components/piano/Piano.svelte
@@ -6,6 +6,7 @@
 	import { blackKeys, whiteKeys, type KeyNote } from "@/lib/piano/keys.ts";
 	import { addPianoKeyboardListeners } from "@/lib/piano/addPianoKeyboardListeners.ts";
 	import PianoSettings from "./PianoSettings.svelte";
+	import PianoKeys from "../PianoKeys.svelte";
 
 	onMount(() => {
 		synthInit();
@@ -14,94 +15,31 @@
 		return () => removeListeners();
 	});
 
-	const keyMouseEnter = (key: KeyNote, octave: number) => () =>
+	const keyMouseEnter = (key: KeyNote, octave: number) =>
 		$keysDown["mouse_left"] && keyDown(`${key}${octave + $octaveOffset}`);
-	const keyMouseDown = (key: KeyNote, octave: number) => () => {
+	const keyMouseDown = (key: KeyNote, octave: number) => {
 		keyDown(`${key}${octave + $octaveOffset}`);
 		keysDown.update((old) => ({ ...old, mouse_left: true }));
+		console.log($keysDown);
 	};
-	const keyMouseUp = (key: KeyNote, octave: number) => () => {
+	const keyMouseUp = (key: KeyNote, octave: number) => {
 		keyUp(`${key}${octave + $octaveOffset}`);
 		keysDown.update((old) => ({ ...old, mouse_left: false }));
 	};
-	const keyMouseLeave = (key: KeyNote, octave: number) => () =>
+	const keyMouseLeave = (key: KeyNote, octave: number) =>
 		$keysDown["mouse_left"] && keyUp(`${key}${octave + $octaveOffset}`);
 </script>
 
 <PianoSettings />
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<div
-	id="piano"
-	on:mouseleave={() => {
+<PianoKeys
+	keysDown={$keysDown}
+	octaveOffset={$octaveOffset}
+	octavesShown={$octavesShown}
+	on:pianoMouseLeave={() => {
 		keysDown.update((old) => ({ ...old, mouse_left: false }));
 	}}
-	class="grid grid-cols-4 w-full"
->
-	{#each new Array($octavesShown) as _, i}
-		<div class="piano-octave-container aspect-video">
-			<div class="piano-white">
-				{#each whiteKeys as key}
-					<button
-						on:mouseenter={keyMouseEnter(key, i)}
-						on:mousedown={keyMouseDown(key, i)}
-						on:mouseup={keyMouseUp(key, i)}
-						on:mouseleave={keyMouseLeave(key, i)}
-						style="grid-area: {key};"
-						class="white-key"
-						class:pressed={$keysDown[`${key}${i + $octaveOffset}`]}
-					></button>
-				{/each}
-			</div>
-			<div class="piano-black">
-				{#each blackKeys as key}
-					<button
-						on:mouseenter={keyMouseEnter(key, i)}
-						on:mousedown={keyMouseDown(key, i)}
-						on:mouseup={keyMouseUp(key, i)}
-						on:mouseleave={keyMouseLeave(key, i)}
-						style="grid-area: {key.replace('#', 's')};"
-						class="black-key"
-						class:pressed={$keysDown[`${key}${i + $octaveOffset}`]}
-					></button>
-				{/each}
-			</div>
-		</div>
-	{/each}
-</div>
-
-<style>
-	#piano {
-		user-select: none;
-	}
-	.piano-octave-container {
-		display: grid;
-		grid-template-columns: 1fr;
-		grid-template-rows: 1fr 1fr 1fr;
-	}
-	.piano-black {
-		grid-area: 1 / 1 / 3 / 2;
-		display: grid;
-		grid-template-areas: ". . Cs Cs . Ds Ds . . . . Fs Fs . Gs Gs . As As . .";
-	}
-	.piano-white {
-		grid-area: 1 / 1 / 4 / 2;
-		display: grid;
-		grid-template-areas: "C C C D D D E E E F F F G G G A A A B B B";
-	}
-	.white-key {
-		background-color: #f5f2e4;
-		border-radius: 0vw 0vw 0.5vw 0.5vw;
-		border: 1px solid #00000040;
-	}
-	.white-key.pressed {
-		background-color: hsl(49, 46%, 98%);
-	}
-	.black-key {
-		background-color: #000000;
-		border-radius: 0vw 0vw 0.5vw 0.5vw;
-		border: 1px solid #ffffff40;
-	}
-	.black-key.pressed {
-		background-color: hsl(0, 0%, 14%);
-	}
-</style>
+	on:keyMouseEnter={(e) => keyMouseEnter(e.detail.key, e.detail.i)}
+	on:keyMouseDown={(e) => keyMouseDown(e.detail.key, e.detail.i)}
+	on:keyMouseUp={(e) => keyMouseUp(e.detail.key, e.detail.i)}
+	on:keyMouseLeave={(e) => keyMouseLeave(e.detail.key, e.detail.i)}
+/>
